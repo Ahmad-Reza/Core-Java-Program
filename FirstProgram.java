566b98c8bdd0243602b72fd795c2a17eb348957c
Ahmad-Reza <ahmadrezambit@gmail.com>
2021-08-22 21:51:50 +0530

    First program

---
+++ FirstProgram.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Hello{
+    public static void main(String[] args){
+         // prints Hello India
+        System.out.println("Hello India");
+    }
+}
+// output: Hello India
+/**
+# Explanation:
+
+1. Comments:  // Single line comment
+              /* Multi line comments*/
+/*
+2. import java.io.*: 
+- This means all the classes of io package can be imported. Java io package provides 
+  a set of input and output streams for reading and writing data to files or other input or output sources.
+
+3. class: The class contains the data and methods to be used in the program
+4. static void Main(): static keyword tells us that this method is accessible without instantiating the class. 
+5. void: keywords tell that this method will not return anything. The main() method is the entry point of our application.
+
+6. System.in: This is the standard input stream that is used to read characters from the keyboard or any other standard input device.
+
+7. System.out: This is the standard output stream that is used to produce the result of a program on an output device like the computer screen.
+
+8. println():  This method in Java is also used to display text on the console.
+    It prints the text on the console and the cursor moves to the start of the next line at the console.
+    The next printing takes place from the next line.
+
+*/
+
