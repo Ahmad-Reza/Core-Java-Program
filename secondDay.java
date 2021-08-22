b74814c0bed06d4b79efbbab8b187b4359a56dac
Ahmad-Reza <ahmadrezambit@gmail.com>
2021-08-22 21:50:08 +0530

    Java termonology

---
+++ secondDay.java
@@ -0,0 +1,73 @@
+/**
+# why java is simple:
+- java is a Plateform independent language
+- there is no concept of pointer
+- there is no concept of memory management in the hand of developer
+    (memory management is done by JVM by GArbage collector).
+- In java all programming component are binded together into a single Unit "Class"
+    programming component(ex: variable, method)
+
+# diff between C++ and java Class:
+a.) Class in C++ is collection of data/variable, function
+   ex: class test{
+        public: // access modifier
+        variable;
+        function;
+    }
+    int main(){
+        //write some codes.
+    }
+b.) Class in Java is collection of variable, method, main.
+   ex: public class test{
+        int var;
+        void animal();
+        main();
+    }
+=> variable - to hold data value
+=> method - to perform action       
+=> main() - to start the execution of program
+
+# Parts/Modules of Java language(james ghosling):-
+    1. java SE(standard edition)/core java
+    2. java EE(Enterprise edition)/advance java
+    3. java ME(micro/machine/mobile edition) - outdated
+
+# core java :- 
+- core java provides the fundamentals building blocks are used in cunstructing 
+  java programing which are also known as programing component(variable, methods, blocks).
+  ex: standalone application, android app etc
+
+# advance java :- JDBC/JSP/Servlet
+- advance java means java program which can integrating with non-java programming components.
+- java program integrating with CUI(console user interface), GUI(graphics user interface), Web.
+
+a) CUI(console user interface): interacts with console input(command prompt) are known as CUI
+b) GUI(graphics user interface): interacts with graphics components are known as GUI
+
+# programing component:
+- variable 
+- methods
+- blocks
+- constructor
+- class
+- interface
+- abstract class
+
+# concept of java:
+- Oops(Object oriented programing structure)
+- Exception handling
+- collection framework
+- GUI programing(AWT/swing/Applet)
+
+c) JDK(java development kit) - in which have java compiler for compilation and JVM for execution.
+    note: in real time developer computer have JDK software
+d) JRE(java run/real time enviroment) - in which have only JVM for execution process(there is no java compiler)
+e) JVM(java virtual machine): which is available in JDK & JRE, this jvm runs independently in the process of executing java byte code(.class)
+    eg: JDK(JRE(development tools(javac, java), JVM(.jar etc)))
+
+f) Virtual Machine - the software component which internally having the behaviour like machine is known as virtual machine.  
+    
+*/
+
+//This is the 45 days of java program in which we are going to cover all java programming components and fundamental concepts.
+
