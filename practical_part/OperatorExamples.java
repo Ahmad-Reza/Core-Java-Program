d0caed324bb7483482e8739cfe56e048016d3477
Ahmad-Reza <ahmadrezambit@gmail.com>
2021-12-01 13:46:09 +0530

    Bitwise operators explaination with examples

--- practical_part/OperatorExamples.java
+++ practical_part/OperatorExamples.java
@@ -60,7 +60,7 @@ public class OperatorExamples {
 
         // 5. Logical Operators:
         // initializing variables
-        int a = 10, b = 20, c = 20;
+        int a = 5, b = 7, c = 20;
   
         // AND Operator && : (return true if all conditions under consideration are satisfied)
         if ((a > b) && (b == c)) {
@@ -81,6 +81,31 @@ public class OperatorExamples {
         // 6. Ternary Operator : result = condition ? true : false; if conditio is true then print true or false
         String result = (a<b) ? "Ahmad Reza" : "ahmad";
         System.out.println(result); // o/p: Ahmad Reza
+
+        // 7. Bitwise operators: used to performing manipulation of individual bits of a number.
+        // They can be used with any of the integral types (char, short, int, etc). 
+ 
+        // Bitwise AND (&) – This operator is a binary operator, denoted by "&". 
+        // It returns bit by bit AND of input values, i.e, if both bits are 1, it gives 1, else it gives 0. 
     
+        System.out.println("a&b = " + (a & b)); // 0101 & 0111=0101 = 5
+ 
+        // Bitwise OR (|) – This operator is a binary operator, denoted by "|". It returns bit by bit OR of input 
+        //values, i.e, if either of the bits is 1, it gives 1, else it gives 0. 
+        
+        System.out.println("a|b = " + (a | b)); // 0101 | 0111=0111 = 7
+ 
+        // Bitwise XOR (^) – This operator is a binary operator, denoted by "^". It returns bit by bit XOR of input 
+        //values, i.e, if corresponding bits are different, it gives 1, else it gives 0. 
+
+        System.out.println("a^b = " + (a ^ b)); // 0101 ^ 0111=0010 = 2
+ 
+        // Bitwise Complement (~) – This operator is a unary operator, denoted by "~". It returns the one’s complement
+        // representation of the input value, i.e, with all bits inverted, which means it makes every 0 to 1, and every 1 to 0. 
+        // ~0101=1010
+        // will give 2's complement of 1010 = -6
+        System.out.println("~a = " + ~a); // -6 
+    
+        // Left & Right shift will cover after
     }
 }
