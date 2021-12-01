33d0b8fcc1aae0f5bd11d77a786eac75d5df8b82
Ahmad-Reza <ahmadrezambit@gmail.com>
2021-12-02 01:21:38 +0530

    One dimensinal array and object type array example

---
+++ practical_part/ArraysProgram.java
@@ -0,0 +1,48 @@
+package practical_part;
+
+public class ArraysProgram {
+     
+    public static void main(String[] args) {
+        // declares an Array of integers.
+        int[] arr; 
+          
+        // allocating memory for 5 integers.
+        arr = new int[5]; 
+            
+        // initialize the first elements of the array
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+            
+        // accessing the elements of the specified array
+        for (int i = 0; i < arr.length; i++)
+            System.out.println("Element at index " + i + " : "+ arr[i]);   
+            
+        // Java program to illustrate creating an array of objects
+        Student[] std; // declaration
+        std = new Student[5]; // allocating memory
+  
+        // initialization...
+        std[0] = new Student(1, "Ahmad"); 
+        std[1] = new Student(2, "vaibhav");
+        std[2] = new Student(3, "shikar");
+        std[3] = new Student(4, "dharmesh");
+        std[4] = new Student(5, "mohit");
+  
+        // accessing the elements of the specified array
+        for (int i = 0; i < std.length; i++)
+            System.out.println("Element at " + i + " : " + std[i].roll_no +" "+ std[i].name);
+
+    }
+}
+
+class Student {
+    public int roll_no;
+    public String name;
+    Student(int roll_no, String name) {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}
